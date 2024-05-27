3998dfc34e6401e5088df0e5d20283a0cfff4666
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-05-27 13:20:56 -0400

    made connections

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
 
 
 }
